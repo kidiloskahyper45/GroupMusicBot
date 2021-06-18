5741cbd362a136c391bc6f756924bb324505c374
Aparichithan <66161708+Aparichithan@users.noreply.github.com>
2021-06-18 19:23:53 +0530

    Update pmpermit.py

--- MusicBot/modules/pmpermit.py
+++ MusicBot/modules/pmpermit.py
@@ -8,6 +8,6 @@ from MusicBot.services.callsmusic.callsmusic import client as USER
 async def pmPermit(client: USER, message: Message):
     await USER.send_message(
         message.chat.id,
-        "**Hi there, This is a music assistant service  @ZK_GvCBot.**\n\n ❗️ **Rules:**\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n **⚠️ Disclamer:** If you are sending a message here it means admin from @tgVCSets will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here.\n\n**Feel Free to Contact Us @ZauteBot.**",
+        "**Hi there, This is a music assistant service  [𝙻𝚄𝙽𝙰](@Luna_vc_bot).**\n\n ❗️ **Rules:**\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **SEND GROUP INVITE LINK OR USERNAME IF USERBOT CAN'T JOIN YOUR GROUP.**\n\n **⚠️ Disclamer:** If you are sending a message here it means admin from @HappinessValley will see your message and join chat\n    - Don't add this user to secret groups.\n   - Don't Share private info here.\n\n**Feel Free to Contact Us [APARICHITHAN](https://t.me/stranger_of_telegram).**",
     )
     return
